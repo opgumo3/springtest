d1200871495aa566610b23513839884c12fa7da7
gumo <opgumo@naver.com>
2023-01-02 15:54:11 +0900

    d

---
+++ src/main/java/com/example/demo3333/Demo3333Application.java
@@ -0,0 +1,13 @@
+package com.example.demo3333;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3333Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo3333Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo3333/Demo3333ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo3333;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo3333ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
